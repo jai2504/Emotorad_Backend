302dbba2d01de01f3ef29387b64c60256fea14ac
jai2504 <rahejajai123@gmail.com>
2023-11-25 14:51:18 +0530

    Project started

---
+++ src/main/java/com/emotorad/contact/EmotoradBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emotorad.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotoradBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotoradBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emotorad/contact/EmotoradBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emotorad.contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotoradBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
